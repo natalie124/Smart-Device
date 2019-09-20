08407767e4a13320786370c214745b28b2aaf180
Натали <t.natalie14@gmail.com>
2019-09-20 19:17:01 +0400

    написала скрипты для окна

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 (function () {
   var Selector = {
+    HEADER: {
+      CONTACTS_BTN: '.header-contacts__btn'
+    },
     FOOTER: {
       DROPDOWN: {
         NAV: '.page-footer__nav'
@@ -10,14 +13,94 @@
     MAIN: {
       CONTENT: '.page_content',
       CROP_TEXT: '.about-company__text-container p:last-child'
-    }
+    },
+    OVERLAY: '.overlay',
+    FEEDBACK_POPUP: '.feedback__popup',
+    FEEDBACK_CLOSE: '.feedback__close'
   };
 
   var ClassName = {
-    CLOSE_FOOTER_DROPDOWN: 'page-footer__dropdown-close'
+    CLOSE_FOOTER_DROPDOWN: 'page-footer__dropdown-close',
+    OPEN_POPUP: 'feedback__popup--open'
+  };
+
+  var Code = {
+    ENTER: 13,
+    ESC: 27,
+    TAB: 9
   };
 
   var CROP_TEXT_SYMBOL = 200;
+
+  var popupOpenBtn = document.querySelector(Selector.HEADER.CONTACTS_BTN);
+  var popupCloseBtn = document.querySelector(Selector.FEEDBACK_CLOSE);
+
+  var popup = document.querySelector(Selector.FEEDBACK_POPUP);
+  var overlay = document.querySelector(Selector.OVERLAY);
+
+  /**
+   * открывает окно
+   *
+   */
+  function openPopup() {
+    if (popup && overlay && ClassName.OPEN_POPUP) {
+      var fieldName = popup.querySelector('input#popup-name');
+
+      popup.classList.add(ClassName.OPEN_POPUP);
+      overlay.classList.add(ClassName.OPEN_POPUP);
+      fieldName.focus();
+      document.addEventListener('keydown', onEscPress);
+    }
+  }
+  /**
+   * закрывает окно
+   *
+   */
+  function closePopup() {
+    if (popup && overlay && ClassName.OPEN_POPUP) {
+      popup.classList.remove(ClassName.OPEN_POPUP);
+      overlay.classList.remove(ClassName.OPEN_POPUP);
+      document.removeEventListener('keydown', onEscPress);
+    }
+  }
+  /**
+   * обрабатывает событие при нажатии  Esc
+   *
+   * @param {object} evt объект события
+   * @param {function}  action действие, когда событие сработает
+   */
+  function isEscEvent(evt, action) {
+    if (evt.keyCode === Code.ESC) {
+      action();
+    }
+  }
+  /**
+   * обрабатывает событие при нажатии  Enter
+   *
+   * @param {object} evt объект события
+   * @param {function}  action действие, когда событие сработает
+   */
+  function isEnterEvent(evt, action) {
+    if (evt.keyCode === Code.ENTER) {
+      action();
+    }
+  }
+  /**
+   * обрабатывает событие при нажатии Esc
+   *
+   * @param {object} evt объект события
+   */
+  function onEscPress(evt) {
+    isEscEvent(evt, closePopup);
+  }
+  /**
+   * обрабатывает событие открытия окна при нажатии Enter
+   *
+   * @param {object} evt объект события
+   */
+  function onPopupOpenBtnEnterPress(evt) {
+    isEnterEvent(evt, openPopup);
+  }
   /**
    * добавляет класс HTML-элементам
    * @param {object} selectors объект с селекторами элементов
@@ -67,4 +150,9 @@
   addСlasses(Selector.FOOTER.DROPDOWN, ClassName.CLOSE_FOOTER_DROPDOWN); // добавляет класс, скрывающий списки в подвале
   addFooterDropdownToggle(Selector.FOOTER.DROPDOWN_TITLE, ClassName.CLOSE_FOOTER_DROPDOWN); // добавляет переключение выпадающих списков  подвале
   truncatesText(Selector.MAIN.CROP_TEXT, CROP_TEXT_SYMBOL); // обрезает текст до 200 символов
+
+  popupOpenBtn.addEventListener('click', openPopup);
+  popupOpenBtn.addEventListener('keydown', onPopupOpenBtnEnterPress);
+  popupCloseBtn.addEventListener('click', closePopup);
+  overlay.addEventListener('click', closePopup);
 })();
